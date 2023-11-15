dbd1decd03ee5f75628dc87b2f46555a7bd7d4c6
Renzo Carpio <renzocarpiop@outlook.com>
2023-11-15 09:50:10 -0500

    vercel prueba 5

--- index.js
+++ index.js
@@ -6,6 +6,9 @@ const app = express();
 
 // Middlewares here 
 app.set('view engine', 'ejs');
+app.set("views", __dirname + "/views");
+app.set("view engine", "ejs");
+app.use(express.static(__dirname + "public"));
 app.use(express.json());
 app.use(cors());
 
